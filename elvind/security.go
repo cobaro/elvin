6047f7fdb5a4516b9844f9e20bdc39ef8ef7dfc9
billsegall <bill@segall.net>
2018-07-02 15:06:17 +1000

    Initial thinking on key security matching

---
+++ elvind/security.go
@@ -0,0 +1,180 @@
+// Copyright 2018 Cobaro Pty Ltd. All Rights Reserved.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package main
+
+import (
+	"bytes"
+	"github.com/cobaro/elvin/elvin"
+)
+
+// Producer scheme notification keys are sent in the clear
+// Consumer scheme subscription keys are sent in the clear
+//
+// As a connection is assumed to be long lived, any connection
+// keys are primed (run through one way function) on arrival
+// and the original discarded
+//
+// As a subscription is assumed to be long lived, any subscription
+// keys are primed (run through one way function) on arrival
+// and the original discarded
+//
+// Connection keys were that way but are stored primed
+// to avoid calculating them every time.
+//
+// FIXME: There is an optimization at some point where for producer
+// and consumer connections keys we might mark (cache)a pair as
+// matching updating as needed.
+
+// Prime (run through one way func) a KeyBlock, which keys is dependent
+// on whether we are a producer or consumer
+func Prime(keys elvin.KeyBlock, producer bool) {
+	for scheme, ksl := range keys {
+		switch scheme {
+		case elvin.KeySchemeSha1Producer:
+			if producer {
+				for i, raw := range ksl[0] {
+					ksl[0][i] = elvin.PrimeSha1(raw)
+				}
+			}
+		case elvin.KeySchemeSha1Consumer:
+			if !producer {
+				for i, raw := range ksl[0] {
+					ksl[0][i] = elvin.PrimeSha1(raw)
+				}
+			}
+		case elvin.KeySchemeSha1Dual:
+			if producer {
+				for i, raw := range ksl[0] {
+					ksl[0][i] = elvin.PrimeSha1(raw)
+				}
+			} else {
+				for i, raw := range ksl[1] {
+					ksl[1][i] = elvin.PrimeSha1(raw)
+				}
+			}
+		case elvin.KeySchemeSha256Producer:
+			if producer {
+				for i, raw := range ksl[0] {
+					ksl[0][i] = elvin.PrimeSha256(raw)
+				}
+			}
+		case elvin.KeySchemeSha256Consumer:
+			if !producer {
+				for i, raw := range ksl[0] {
+					ksl[0][i] = elvin.PrimeSha256(raw)
+				}
+			}
+		case elvin.KeySchemeSha256Dual:
+			if producer {
+				for i, raw := range ksl[0] {
+					ksl[0][i] = elvin.PrimeSha256(raw)
+				}
+			} else {
+				for i, raw := range ksl[1] {
+					ksl[1][i] = elvin.PrimeSha256(raw)
+				}
+			}
+		}
+	}
+}
+
+// Do the keys match
+// i.e, amongst the notifications and producer's keys does one of
+// our schemes succeed agains the subscriptions and consumer's keys.
+func SecurityMatches(nfn elvin.NotifyEmit, sub elvin.Subscription, pKeys, cKeys elvin.KeyBlock) bool {
+
+	// Start with the simple (and hence common) cases
+
+	// No-one cares
+	if nfn.DeliverInsecure && sub.AcceptInsecure {
+		return true
+	}
+
+	// No producer keys, so determined by subscriber
+	if len(nfn.Keys) == 0 && len(pKeys) == 0 {
+		return sub.AcceptInsecure
+	}
+
+	// No consumer keys so determined by producer
+	if len(sub.Keys) == 0 && len(cKeys) == 0 {
+		return nfn.DeliverInsecure
+	}
+
+	// We got here cos we have to deal with keys
+
+	// We could merge togther the connection and notification/subscription
+	// keys but it's simpler just to run the combinations
+
+	if len(nfn.Keys) > 0 {
+		// Prime the notification's keys
+		Prime(nfn.Keys, true)
+
+		// A Match is always true
+		if KeyBlocksMatches(nfn.Keys, sub.Keys) || KeyBlocksMatches(nfn.Keys, cKeys) {
+			return true
+		}
+	}
+	if len(pKeys) > 0 {
+		// A Match is always true
+		if KeyBlocksMatches(pKeys, sub.Keys) || KeyBlocksMatches(pKeys, cKeys) {
+			return true
+		}
+	}
+
+	// No matches so it shall not not pass
+	return false
+}
+
+func KeyBlocksMatches(producer, consumer elvin.KeyBlock) bool {
+	for scheme, ksl := range producer {
+		switch scheme {
+		case elvin.KeySchemeSha1Dual:
+			if KeySetMatches(ksl[0], consumer[elvin.KeySchemeSha1Dual][1]) && KeySetMatches(ksl[1], consumer[elvin.KeySchemeSha1Dual][0]) {
+				return true
+			}
+		case elvin.KeySchemeSha1Producer:
+			if KeySetMatches(ksl[0], consumer[elvin.KeySchemeSha1Producer][0]) {
+				return true
+			}
+		case elvin.KeySchemeSha1Consumer:
+			if KeySetMatches(ksl[0], consumer[elvin.KeySchemeSha1Consumer][0]) {
+				return true
+			}
+		case elvin.KeySchemeSha256Dual:
+		case elvin.KeySchemeSha256Producer:
+		case elvin.KeySchemeSha256Consumer:
+		}
+	}
+
+	return false
+}
+
+// A match occurs if there is a match across any of the two sets of keys
+func KeySetMatches(first, second elvin.KeySet) bool {
+	for _, f := range first {
+		for _, s := range second {
+			if bytes.Equal(f, s) {
+				return true
+			}
+		}
+	}
+	return false
+}
---
+++ elvind/security_test.go
@@ -0,0 +1,148 @@
+// Copyright 2018 Cobaro Pty Ltd. All Rights Reserved.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"github.com/cobaro/elvin/elvin"
+	"testing"
+)
+
+var k1 = []byte("foo")
+var k2 = []byte("bar")
+var k3 = []byte("baz")
+
+var k1SHA1, k2SHA1, k3SHA1 []byte
+
+var k1SHA256, k2SHA256, k3SHA256 []byte
+
+func init() {
+	// Hash the keys
+	k1SHA1 = elvin.PrimeSha1(k1)
+	k2SHA1 = elvin.PrimeSha1(k2)
+	k3SHA1 = elvin.PrimeSha1(k3)
+
+	k1SHA256 = elvin.PrimeSha256(k1)
+	k2SHA256 = elvin.PrimeSha256(k2)
+	k3SHA256 = elvin.PrimeSha256(k3)
+}
+
+// Test the Prime function used to convert keys in the router
+func TestPrime(t *testing.T) {
+
+	// SHA1 Producer
+	var producerKeySet elvin.KeySet
+	producerKeySet = append(producerKeySet, k1)
+	producerKeySetList := elvin.KeySetList{producerKeySet}
+	producerKeyBlock := make(map[int]elvin.KeySetList)
+	producerKeyBlock[elvin.KeySchemeSha1Producer] = producerKeySetList
+
+	// Should not change the producer key
+	Prime(producerKeyBlock, false)
+	if !bytes.Equal(k1, producerKeyBlock[elvin.KeySchemeSha1Producer][0][0]) {
+		t.Fatalf("Prime() wrongly changed a producer key (%s->%s)", hex.EncodeToString(k1), hex.EncodeToString(producerKeyBlock[elvin.KeySchemeSha1Producer][0][0]))
+	}
+
+	// Should change the producer key
+	Prime(producerKeyBlock, true)
+	if bytes.Equal(k1, producerKeyBlock[elvin.KeySchemeSha1Producer][0][0]) {
+		t.Fatalf("Prime() didn't change the producer key (%s->%s)", hex.EncodeToString(k1), hex.EncodeToString(producerKeyBlock[elvin.KeySchemeSha1Producer][0][0]))
+	}
+	if !bytes.Equal(producerKeyBlock[elvin.KeySchemeSha1Producer][0][0], k1SHA1) {
+		t.Fatalf("Prime() changes the producer key to a bad value:\n%s\n%s", hex.EncodeToString(k1SHA1), hex.EncodeToString(producerKeyBlock[elvin.KeySchemeSha1Producer][0][0]))
+	}
+
+	// SHA1 Consumer
+	var consumerKeySet elvin.KeySet
+	consumerKeySet = append(consumerKeySet, k1)
+	consumerKeySetList := elvin.KeySetList{consumerKeySet}
+	consumerKeyBlock := make(map[int]elvin.KeySetList)
+	consumerKeyBlock[elvin.KeySchemeSha1Consumer] = consumerKeySetList
+
+	// Should not change the consumer key
+	Prime(consumerKeyBlock, true)
+	if !bytes.Equal(k1, consumerKeyBlock[elvin.KeySchemeSha1Consumer][0][0]) {
+		t.Fatalf("Prime() wrongly changed a consumer key (%s->%s)", hex.EncodeToString(k1), hex.EncodeToString(consumerKeyBlock[elvin.KeySchemeSha1Consumer][0][0]))
+	}
+
+	// Should change the consumer key
+	Prime(consumerKeyBlock, false)
+	if bytes.Equal(k1, consumerKeyBlock[elvin.KeySchemeSha1Consumer][0][0]) {
+		t.Fatalf("Prime() didn't change the consumer key (%s->%s)", hex.EncodeToString(k1), hex.EncodeToString(consumerKeyBlock[elvin.KeySchemeSha1Consumer][0][0]))
+	}
+	if !bytes.Equal(consumerKeyBlock[elvin.KeySchemeSha1Consumer][0][0], k1SHA1) {
+		t.Fatalf("Prime() changes the consumer key to a bad value:\n%s\n%s", hex.EncodeToString(k1SHA1), hex.EncodeToString(consumerKeyBlock[elvin.KeySchemeSha1Consumer][0][0]))
+	}
+
+	// SHA1 Dual
+	producerKeySet = nil // reset
+	consumerKeySet = nil
+	producerKeySet = append(producerKeySet, k1)
+	consumerKeySet = append(consumerKeySet, k1)
+	keySetList := elvin.KeySetList{producerKeySet, consumerKeySet}
+
+	keyBlock := make(map[int]elvin.KeySetList)
+
+	keyBlock[elvin.KeySchemeSha1Dual] = keySetList
+
+	// Should change the consumer but not the producer
+	Prime(keyBlock, false)
+	if bytes.Equal(keyBlock[elvin.KeySchemeSha1Dual][0][0], keyBlock[elvin.KeySchemeSha1Dual][1][0]) {
+		t.Fatalf("Prime() wrongly changed a producer key (%s->%s)", hex.EncodeToString(k1), hex.EncodeToString(keyBlock[elvin.KeySchemeSha1Dual][0][0]))
+	}
+
+	// Should change the producer not the consumer so now equal
+	Prime(keyBlock, true)
+	if !bytes.Equal(keyBlock[elvin.KeySchemeSha1Dual][0][0], keyBlock[elvin.KeySchemeSha1Dual][1][0]) {
+		t.Fatalf("Producer and consumer show now match %s,%s", hex.EncodeToString(keyBlock[elvin.KeySchemeSha1Dual][0][0]), hex.EncodeToString(keyBlock[elvin.KeySchemeSha1Dual][0][1]))
+	}
+
+	fmt.Printf("%+v\n", keyBlock[elvin.KeySchemeSha1Dual][0][0])
+	fmt.Printf("%+v\n", keyBlock[elvin.KeySchemeSha1Dual][1][0])
+
+	// SHA256 Dual
+	producerKeySet = nil // reset
+	consumerKeySet = nil
+	producerKeySet = append(producerKeySet, k1)
+	consumerKeySet = append(consumerKeySet, k1)
+	keySetList = elvin.KeySetList{producerKeySet, consumerKeySet}
+
+	keyBlock = make(map[int]elvin.KeySetList)
+
+	keyBlock[elvin.KeySchemeSha256Dual] = keySetList
+
+	// Should change the consumer but not the producer
+	Prime(keyBlock, false)
+	if bytes.Equal(keyBlock[elvin.KeySchemeSha256Dual][0][0], keyBlock[elvin.KeySchemeSha256Dual][1][0]) {
+		t.Fatalf("Prime() wrongly changed a producer key (%s->%s)", hex.EncodeToString(k1), hex.EncodeToString(keyBlock[elvin.KeySchemeSha256Dual][0][0]))
+	}
+
+	// Should change the producer not the consumer so now equal
+	Prime(keyBlock, true)
+	if !bytes.Equal(keyBlock[elvin.KeySchemeSha256Dual][0][0], keyBlock[elvin.KeySchemeSha256Dual][1][0]) {
+		t.Fatalf("Producer and consumer show now match %s,%s", hex.EncodeToString(keyBlock[elvin.KeySchemeSha256Dual][0][0]), hex.EncodeToString(keyBlock[elvin.KeySchemeSha256Dual][0][1]))
+	}
+
+	fmt.Printf("%+v\n", keyBlock[elvin.KeySchemeSha256Dual][0][0])
+	fmt.Printf("%+v\n", keyBlock[elvin.KeySchemeSha256Dual][1][0])
+}
